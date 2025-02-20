6be15c930a14c16a90f1101c4a4a3359b9912d1e
SonuMunda <sonumunda1312.gmail.com>
2025-02-20 10:37:09 +0530

    gauth

--- src/auth/GoogleAuthLogin.jsx
+++ src/auth/GoogleAuthLogin.jsx
@@ -21,7 +21,7 @@ const GoogleAuthLogin = () => {
 
 
   return (
-    <div className="google-login-btn">
+    <div className="google-login-btn w-full">
       <GoogleLogin
         onSuccess={(credentialResponse) => {
           handleLogin(credentialResponse);
@@ -29,6 +29,7 @@ const GoogleAuthLogin = () => {
         onError={() => {
           toast.error("Login failed");
         }}
+        width={"100%"}
       />
     </div>
   );
